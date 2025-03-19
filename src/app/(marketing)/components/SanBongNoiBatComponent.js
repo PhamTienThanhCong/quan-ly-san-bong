d276ecc66e908d973df52f1c9fd071c6fc02b47a
Phạm Tiến Thành Công <cong.pttc@gmail.com>
2025-03-19 11:45:11 +0700

    ok

--- src/app/(marketing)/components/SanBongNoiBatComponent.js
+++ src/app/(marketing)/components/SanBongNoiBatComponent.js
@@ -24,7 +24,7 @@ const SanBongNoiBatComponent = () => {
     <div className="container-fluid contact">
       <div className="container pb-5">
         <div className="text-center mx-auto pb-5 wow fadeInUp" data-wow-delay="0.2s" style={{ maxWidth: "800px" }}>
-          <h1 className="display-5 mb-4">Các sâm tiêu biểu</h1>
+          <h1 className="display-5 mb-4">Các sân tiêu biểu</h1>
           <p className="mb-0">
             Chúng tôi cung cấp các sân chất lượng, đáp ứng mọi nhu cầu của bạn, giúp bạn có những trận đấu tuyệt vời
             nhất.
